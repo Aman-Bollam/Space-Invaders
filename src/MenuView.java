322f04445567a27fb84a97703d6c67a3b7128514
Reshad05 <reshadayman@gmail.com>
2023-03-24 11:59:24 -0400

    Added more images and some fnctionality to playership

--- src/MenuView.java
+++ src/MenuView.java
@@ -9,8 +9,8 @@ import javax.swing.JPanel;
 public class MenuView extends JPanel implements MouseListener, MouseMotionListener{
 	private boolean settings;
 	private boolean skins;
-	private int slideCounter = 0;
-	private String [] imageSlides = new String[]{};
+	private int slideCounter;
+	private String [] imageSlides = {"slide1.png","slide2.png","slide3.png"};
 	private Image display;
 	private final String path = "images\\";
 	private boolean mouseHoveredPlay;
@@ -18,11 +18,11 @@ public class MenuView extends JPanel implements MouseListener, MouseMotionListen
 	private boolean mouseHoveredSkins;
 	private boolean hoveredNext;
 	private boolean hoveredEquip;
-	private boolean back;
 	private boolean hoveredBack;
-	private boolean menuButt;
 	private boolean menuButtHovered;
+	private PlayerShip ship;
 	public MenuView() {
+		slideCounter = 0;
 		settings = false;
 		skins = false;
 		mouseHoveredPlay = false;
@@ -30,6 +30,9 @@ public class MenuView extends JPanel implements MouseListener, MouseMotionListen
 		mouseHoveredSkins = false;
 		hoveredNext = false;
 		hoveredEquip= false;
+		hoveredBack=false;
+		menuButtHovered = false;
+		// ship = new PlayerShip(0);
 		this.setPreferredSize(new Dimension(960,960));
 		this.setFocusable(true);
 		this.addMouseListener(this);
@@ -50,13 +53,33 @@ public class MenuView extends JPanel implements MouseListener, MouseMotionListen
 			}
 			g.drawImage(display, 0, 0, null);
 		} else if(skins){
-			g.drawImage(new ImageIcon(path+"skinsbackground.png").getImage(),0,0,null);
-			// g.drawImage(new ImageIcon(path+imageSlides[slideCounter]).getImage(),0,100,null);
-			// g.drawImage(new ImageIcon(path+"next.png").getImage(),518,622,null);
+			g.drawImage(new ImageIcon(path+"BlackHole.png").getImage(),0,0,null);
+			g.drawImage(new ImageIcon(path+imageSlides[slideCounter]).getImage(),0,100,null);
+			if(hoveredNext && slideCounter<8){
+				g.drawImage(new ImageIcon(path+"nexthovered.png").getImage(),518,622,null);
+			} else if(slideCounter<8) {
+				g.drawImage(new ImageIcon(path+"next.png").getImage(),518,622,null);
+			} 	
+			if(hoveredBack && slideCounter>0){
+				g.drawImage(new ImageIcon(path+"backhovered.png").getImage(),320,625,null);
+			} else if(slideCounter>0) {
+				g.drawImage(new ImageIcon(path+"back.png").getImage(),320,625,null);
+			}
+			if(menuButtHovered){
+				g.drawImage(new ImageIcon(path+"menubuttonhover.png").getImage(),158,556,null);
+			} else {
+				g.drawImage(new ImageIcon(path+"menubutton.png").getImage(),158,556,null);
+			}
+			// if(ship.isEquipped(slideCounter)) {
+			// 	g.drawImage(new ImageIcon(path+"equipped.png").getImage(),159,347,null);
+			// } else if(hoveredEquip) {
+			// 	g.drawImage(new ImageIcon(path+"equiphovered.png").getImage(),159,347,null);
+			// } else{
+			// 	g.drawImage(new ImageIcon(path+"equip.png").getImage(),159,347,null);
+			// }
 			
 		}
-		g.drawImage(new ImageIcon(path+"slide1.png").getImage(),0,100,null);
-		g.drawImage(new ImageIcon(path+"equip.png").getImage(),160,347,null);
+		// g.drawImage(new ImageIcon(path+"slide1.png").getImage(),0,100,null);
 	}
 	@Override
 	public void mouseDragged(MouseEvent e) {
@@ -91,33 +114,48 @@ public class MenuView extends JPanel implements MouseListener, MouseMotionListen
 				hoveredEquip = true;
 			} else {
 				hoveredEquip = false;
-			}
+			} 
+			if(e.getX()>=319 && e.getX()<=513 && e.getY()>=624 && e.getY()<=696 && slideCounter>0) {
+				hoveredBack = true;
+			} else {
+				hoveredBack = false;
+			} 
+			if(e.getX()>=156 && e.getX()<=482 && e.getY()>=557 && e.getY()<=611) {
+				menuButtHovered = true;
+			} else {
+				menuButtHovered = false;
+			} 
 			this.repaint();
 		}
 		// TODO Auto-generated method stub
 	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println("X:" + e.getX());
-		System.out.println("Y:" + e.getY());
+		// System.out.println("X:" + e.getX());
+		// System.out.println("Y:" + e.getY());
 		if(!skins && !settings){
 			if(e.getX()>=325 && e.getX()<=637 && e.getY()>=558 && e.getY()<=678){
 			
 			} else if(e.getX()>=397 && e.getX()<=559 && e.getY()>=714 && e.getY()<=768) {
 				skins = true;
-				slideCounter++;
 				this.repaint();
 			} else if(e.getX()>=347 && e.getX()<=611 && e.getY()>=807 && e.getY()<=863) {
 				settings = true;
 				this.repaint();
 			}
 		} else if(skins){
-			if(e.getX()>=518 && e.getX()<=813 && e.getY()>=621 && e.getY()<=694){
+			if(e.getX()>=518 && e.getX()<=813 && e.getY()>=621 && e.getY()<=694 && slideCounter<9) {
 				slideCounter++;
 				this.repaint();
-			} else if(e.getX()>=158 && e.getX()<=412 && e.getY()>=346 && e.getY()<=418){
+			} else if(e.getX()>=158 && e.getX()<=412 && e.getY()>=346 && e.getY()<=418) {
 				// gets player ship
-			} 
+			} else if(e.getX()>=319 && e.getX()<=513 && e.getY()>=624 && e.getY()<=696 && slideCounter>0) {
+				slideCounter--;
+				this.repaint();
+			} else if(e.getX()>=156 && e.getX()<=482 && e.getY()>=557 && e.getY()<=611){
+				skins = false;
+				this.repaint();
+			}
 		}	
 		// TODO Auto-generated method stub
 	}
--- src/PlayerShip.java
+++ src/PlayerShip.java
@@ -1,6 +1,23 @@
 public class PlayerShip {
-    private boolean [] equipped = new boolean[9];
-    public PlayerShip(){
-
+    private boolean [] equipped = {false,false,false,false,false,false,false,false};
+    private String [] ships = {};
+    private String ship;
+    public PlayerShip(int num) {
+        ship = ships[num];
+        for(int i=0; i<9; i++) {
+            if(equipped[i]){
+                equipped[i] = false;
+            }
+        }
+        equipped[num]=true;
+    }
+    public boolean isEquipped(int num) {
+        if(equipped[num]){
+            return true;
+        }
+        return false;
+    }
+    public String getName() {
+        return ship;
     }
 }
