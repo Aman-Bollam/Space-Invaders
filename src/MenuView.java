7df7b99a6517d2744e9b1a380a173ab34b123edd
Aman-Bollam <72667729+Aman-Bollam@users.noreply.github.com>
2023-03-29 12:54:48 -0400

    menu stuff

--- src/MenuView.java
+++ src/MenuView.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
 import javax.swing.*;
 
 public class MenuView extends JPanel implements MouseListener, MouseMotionListener{
