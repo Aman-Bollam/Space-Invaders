dd2050907867a97152be9c898e025fd2257929a1
Reshad05 <reshadayman@gmail.com>
2023-04-17 12:37:03 -0400

    grid

--- src/EnemyGrid.java
+++ src/EnemyGrid.java
@@ -7,6 +7,16 @@ public class EnemyGrid {
         eneY = 85;
         for(int i=0, j=eneX; i<10; i++, j=j+85) {
             enemies[0][i] = new Enemy(3, true, j, eneY);
+            enemies[1][i] = new Enemy(2, true, j, eneY+85);
+            enemies[2][i] = new Enemy(2, true, j, eneY+170);
+            enemies[3][i] = new Enemy(1, true, j, eneY+255);
+            enemies[4][i] = new Enemy(1, true, j, eneY+340);
+        }
+    }
+    public void setEnemies(Enemy [][] grid) {
+        for(int i=0, j=eneX; i<10; i++, j=j+85) {
+            boolean life = enemies[0][i].getLife();
+            enemies[0][i] = new Enemy(3, life, j, eneY);
         }
         for(int i=0, j=eneX; i<10; i++, j=j+85){
             enemies[2][i] = new Enemy(2, true, j, eneY+170);
@@ -18,20 +28,16 @@ public class EnemyGrid {
             enemies[4][i] = new Enemy(1, true, j, eneY+340);
         }
     }
-    public void setEnemies(Enemy [][] grid) {
+    public Enemy[][] getGrid() {
+        Enemy[][] myEnemies = new Enemy[5][11];
         for(int i=0, j=eneX; i<10; i++, j=j+85) {
-            boolean life = enemies[0][i]
-            enemies[0][i] = new Enemy(3, true, j, eneY);
+            myEnemies[0][i] = enemies[0][i];
+            myEnemies[1][i] = enemies[1][i];
+            myEnemies[2][i] = enemies[2][i];
+            myEnemies[3][i] = enemies[3][i];
+            myEnemies[4][i] = enemies[4][i];
         }
-        for(int i=0, j=eneX; i<10; i++, j=j+85){
-            enemies[2][i] = new Enemy(2, true, j, eneY+170);
-        }
-        for(int i=0, j=eneX; i<10; i++, j=j+85){
-            enemies[3][i] = new Enemy(1, true, j, eneY+255);
-        }
-        for(int i=0, j=eneX; i<10; i++, j=j+85){
-            enemies[4][i] = new Enemy(1, true, j, eneY+340);
-        }
+        return myEnemies;
     }
     public void setPosRight() {
         eneX+=2;
