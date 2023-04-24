90763aa21988b3d9e16b590078a78b60d7ed4d22
Reshad05 <reshadayman@gmail.com>
2023-04-24 10:32:17 -0400

    shield ship class

--- src/ShieldShip.java
+++ src/ShieldShip.java
@@ -4,7 +4,24 @@ public class ShieldShip {
     private int health;
     private Rectangle hitbox;
     private boolean life;
+    private String name;
     public ShieldShip() {
         health = 140;
+        name = "ship1.png";
+        life = true;
+    }
+    public void setHealth() {
+        int num;
+        if(health>0) {
+            health-=10;
+        }
+        num = (14)-(((health)/10)-1);
+        name = "ship"+num+".png";
+    }
+    public boolean getLife() {
+        return life;
+    }
+    public void setLife(boolean alive) {
+        life = alive;
     }
 }
